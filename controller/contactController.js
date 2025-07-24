2b6bb61821352a00762288ffdd67e0ece149fad7
dkeum <dkeum@sfu.ca>
2025-07-24 15:25:57 -0700

    making scalable

---
+++ controller/contactController.js
@@ -0,0 +1,47 @@
+const asyncHandler = require("express-async-handler");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+
+// @ POST
+// ROUTE: /email
+
+//SEND an send email with with full name and description
+const sendEmail = asyncHandler(async (req, res) => {
+  const { email, fullName, message } = req.body;
+
+  res.setHeader("Access-Control-Allow-Origin", "https://mathamagic.vercel.app");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_APP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: `${email}`,
+      to: process.env.EMAIL_RECEIVER,
+      subject: `New Contact Form Submission from ${fullName}`,
+      text: `You received a message from ${fullName} (${email}):\n\n${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    return res.status(200).json({ message: "Email sent successfully" });
+  } catch (err) {
+    console.error("Error sending email:", err);
+    return res.status(500).json({ error: "Failed to send email" });
+  }
+});
+
+module.exports = {
+  sendEmail,
+};
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const cors = require("cors");
 
-const nodemailer = require("nodemailer");
 const express = require("express");
 const app = express();
 
@@ -20,52 +19,11 @@ app.use(cors({
 app.use(express.json());
 const PORT = 3000;
 
-app.get("/email", (req, res) => {
-  res.send("Email Received");
-});
 
-app.options("/email", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://mathamagic.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  return res.status(204).end(); // no content
-});
 
-app.post("/email", async (req, res) => {
-  const { email, fullName, message } = req.body;
+app.use("/",require("./routes/publicAPI/contactRoute"))
 
-  res.setHeader("Access-Control-Allow-Origin", "https://mathamagic.vercel.app");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (!email) {
-    return res.status(400).json({ error: "Email is required" });
-  }
-
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_APP_PASSWORD,
-      },
-    });
-
-    const mailOptions = {
-      from: `${email}`,
-      to: process.env.EMAIL_RECEIVER,
-      subject: `New Contact Form Submission from ${fullName}`,
-      text: `You received a message from ${fullName} (${email}):\n\n${message}`,
-    };
-
-    await transporter.sendMail(mailOptions);
-
-   return  res.status(200).json({ message: "Email sent successfully" });
-  } catch (err) {
-    console.error("Error sending email:", err);
-    return res.status(500).json({ error: "Failed to send email" });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`App is listening on port: ${PORT}`);

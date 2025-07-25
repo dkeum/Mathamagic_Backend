af19a64cac386684c1ca306612c26d67e7ac0ace
dkeum <dkeum@sfu.ca>
2025-07-25 02:44:18 -0700

    adding options for vercel

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,9 +3,26 @@ const router = express.Router();
 
 const authController = require("../controller/authController");
 
-router
-  .post("/signup", authController.signUp)
-  .post("/login", authController.login)
-  .get("/logout", authController.logOut);
+// Middleware to set CORS headers
+const setCorsHeaders = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://mathamagic.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+};
+
+// Apply CORS headers to all routes including OPTIONS
+router.use(setCorsHeaders);
+
+// OPTIONS handlers for preflight requests
+router.options("/signup", (req, res) => res.sendStatus(204));
+router.options("/login", (req, res) => res.sendStatus(204));
+router.options("/logout", (req, res) => res.sendStatus(204));
+
+// Actual route handlers
+router.post("/signup", authController.signUp);
+router.post("/login", authController.login);
+router.get("/logout", authController.logOut);
 
 module.exports = router;

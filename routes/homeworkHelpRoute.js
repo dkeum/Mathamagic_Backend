09debe834a9f84eb7a39665da3f013bd75832201
dkeum <dkeum@sfu.ca>
2025-09-07 20:41:18 -0700

    testing

--- routes/homeworkHelpRoute.js
+++ routes/homeworkHelpRoute.js
@@ -4,28 +4,28 @@ const router = express.Router();
 const homeworkHelpController = require("../controller/homeworkHelpController");
 
 // Only apply CORS headers and OPTIONS handlers in non-development environments
-if (process.env.NODE_ENV !== "DEVELOPMENT") {
-  const setCorsHeaders = (req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://mathamagic.vercel.app"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    next();
-  };
+// if (process.env.NODE_ENV !== "DEVELOPMENT") {
+//   const setCorsHeaders = (req, res, next) => {
+//     res.setHeader(
+//       "Access-Control-Allow-Origin",
+//       "https://mathamagic.vercel.app"
+//     );
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     next();
+//   };
 
-  router.use(setCorsHeaders);
+//   router.use(setCorsHeaders);
 
-  // OPTIONS handler for preflight requests
-  router.options("/homework-help/upload-pdf", (req, res) =>
-    res.sendStatus(204)
-  );
-  router.options("/homework-help/upload-image", (req, res) =>
-    res.sendStatus(204)
-  );
-}
+//   // OPTIONS handler for preflight requests
+//   router.options("/homework-help/upload-pdf", (req, res) =>
+//     res.sendStatus(204)
+//   );
+//   router.options("/homework-help/upload-image", (req, res) =>
+//     res.sendStatus(204)
+//   );
+// }
 
 // Actual route handler
 // router.post(

d52e3390ec7dbcf60d50315532fb787a02fcb6cf
dkeum <dkeum@sfu.ca>
2025-09-07 19:06:47 -0700

    testing questionRoute

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const PORT = 3000;
 app.use("/", require("./routes/publicAPI/contactRoute"));
 app.use("/", require("./routes/authRoute")); // Good for Serverless function
 app.use("/", require("./routes/userRoute"))
-// app.use("/", require("./routes/questionRoute"))
+app.use("/", require("./routes/questionRoute"))
 // app.use("/", require("./routes/homeworkHelpRoute"))
 
 

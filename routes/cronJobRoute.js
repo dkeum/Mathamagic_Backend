7a140621d9a8227081b731d58789be70748e6adc
Daniel Keum <dkeum@sfu.ca>
2026-07-14 19:00:27 -0700

    cron job

--- routes/cronJobRoute.js
+++ routes/cronJobRoute.js
@@ -5,17 +5,17 @@ const { cleanUpOldVideos } = require("../api/cron/cleanupvideos");
 
 const {refreshFxRate} = require("../api/cron/refresh-fx-rate")
 
-if (process.env.NODE_ENV !== "DEVELOPMENT") {
-    const setCorsHeaders = (req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "https://mathmagick.com");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Added Authorization for the cron secret
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        next();
-    };
+// if (process.env.NODE_ENV !== "DEVELOPMENT") {
+//     const setCorsHeaders = (req, res, next) => {
+//         res.setHeader("Access-Control-Allow-Origin", "https://mathmagick.com");
+//         res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Added Authorization for the cron secret
+//         res.setHeader("Access-Control-Allow-Credentials", "true");
+//         next();
+//     };
 
-    router.use(setCorsHeaders);
-}
+//     router.use(setCorsHeaders);
+// }
 
 
 // Vercel Cron Endpoint
